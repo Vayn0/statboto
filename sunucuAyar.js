af521ef4c86e5f37e86ce7cb5b6e4c49dd3ccd22
Vayn0 <76044876+Vayn0@users.noreply.github.com>
2020-12-29 19:21:06 +0300

    statboto

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '786868630658089000', // sunucu idsi
+  ownerRole:"792815869683171388", // sunucu sahiplerinin rolü
+  teyitsizRolleri:["792815908434870302"], // kayıt olmayanlara verieln rol
+  jailRolu:"792815908871602177", // cezalılara verilen rol
+  enAltYetkiliRolu:["792815888009003008"], // ilk yetkili permi
+  activity:"aksmcas", // botun durumu
+  status:"idle",
+  invitelink:"https://discord.gg/Q26bp6723j", //davet linki
+  symbol:"る", // tag
+  voicechannel:"792815953871110164" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
